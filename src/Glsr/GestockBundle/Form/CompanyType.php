3556e219c7c401ae295206e44e1ddee3f6314848
Dev-Int <lq@dev-int.net>
2014-05-24 15:44:35 +0200

    Create Company Entity and FormType

---
+++ src/Glsr/GestockBundle/Form/CompanyType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Glsr\GestockBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CompanyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',    'text')
+            ->add('status',  'text')
+            ->add('adress',  'text')
+            ->add('zipcode', 'text')
+            ->add('town',    'text')
+            ->add('phone',   'text')
+            ->add('fax',     'text')
+            ->add('mail',    'email', array(
+                'trim' => TRUE
+            ))
+            ->add('contact', 'text')
+            ->add('gsm',     'text')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Glsr\GestockBundle\Entity\Company'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'glsr_gestockbundle_company';
+    }
+}
