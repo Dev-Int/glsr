3d3684b45ed904be0109764f26bd8961e7710d14
Dev-Int <lq@dev-int.net>
2014-08-26 12:46:58 +0200

    Install KnpSnappyBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new Ornicar\AkismetBundle\OrnicarAkismetBundle(),
             new Misd\PhoneNumberBundle\MisdPhoneNumberBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
