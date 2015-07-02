fbf8329b0ba3274d6b0a533512b11760b75e1d1e
Dev-Int <lq@dev-int.net>
2015-07-02 15:27:22 +0200

    Reduce functions in ArticleController.php
     - Cut too long functions
     - Change route
     - Create GetSession class
     - Add this class in service

--- src/Glsr/GestockBundle/Controller/ArticleController.php
+++ src/Glsr/GestockBundle/Controller/ArticleController.php
@@ -20,6 +20,7 @@ use Glsr\GestockBundle\Entity\Article;
 use Glsr\GestockBundle\Entity\Supplier;
 use Glsr\GestockBundle\Form\ArticleType;
 use Glsr\GestockBundle\Form\ArticleReassignType;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * class ArticleController.
@@ -29,15 +30,16 @@ use Glsr\GestockBundle\Form\ArticleReassignType;
 class ArticleController extends Controller
 {
     /**
-     * indexAction affiche la liste des articles (pagination).
+     * Affiche la liste des articles (pagination).
      *
      * @param int $page numéro de page
      *
-     * @return type
+     * @return Response
      */
     public function indexAction($page)
     {
-        // On récupère le nombre d'article par page depuis un paramètre du conteneur
+        // On récupère le nombre d'article par page
+        // depuis un paramètre du conteneur
         // cf app/config/parameters.yml
         $nbPerPage = $this->container->getParameter('glsr.nb_per_page');
 
@@ -50,69 +52,28 @@ class ArticleController extends Controller
             'GlsrGestockBundle:Gestock/Article:index.html.twig',
             array(
                 'articles' => $articles,
-                'page' => $page,
-                'nb_page' => ceil(count($articles) / $nbPerPage) ?: 1,
+                'page'     => $page,
+                'nb_page'  => ceil(count($articles) / $nbPerPage) ?: 1,
             )
         );
     }
 
     /**
-     * addAction Ajoute un article.
+     * Ajoute un article.
      *
-     * @return type
+     * @return Response
+     *
+     * @throws AccessDeniedException
      */
-    public function addAction()
+    public function addShowAction()
     {
         if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            // On définit un message flash
-            $this->get('session')
-                ->getFlashBag()
-                ->add('info', 'Vous devez être connecté pour accéder à cette page.');
-
-            // On redirige vers la page de connexion
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+            throw new AccessDeniedException();
         }
         $article = new Article();
 
         // On crée le formulaire grâce à l'ArticleType
         $form = $this->createForm(new ArticleType(), $article);
-
-        // On récupère la requête
-        $request = $this->getRequest();
-
-        // On vérifie qu'elle est de type POST
-        if ($request->getMethod() == 'POST') {
-            // On fait le lien Requête <-> Formulaire
-            $form->bind($request);
-
-            // On vérifie que les valeurs rentrées sont correctes
-            if ($form->isValid()) {
-                // On enregistre l'objet $article dans la base de données
-                $etm = $this->getDoctrine()->getManager();
-                $etm->persist($article);
-                $etm->flush();
-
-                // On définit un message flash
-                $this->get('session')
-                    ->getFlashBag()
-                    ->add('info', 'Article bien ajouté');
-
-                // On redirige vers la page de visualisation
-                //  de l'article nouvellement créé
-                return $this->redirect(
-                    $this->generateUrl(
-                        'glstock_art_show',
-                        array('name' => $article->getName())
-                    )
-                );
-            }
-        }
-
-        // À ce stade :
-        // - soit la requête est de type GET,
-        // donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - soit la requête est de type POST,
-        // mais le formulaire n'est pas valide, donc on l'affiche de nouveau
 
         return $this->render(
             'GlsrGestockBundle:Gestock/Article:add.html.twig',
@@ -123,29 +84,59 @@ class ArticleController extends Controller
     }
 
     /**
-     * editAction Modification d'un article.
+     * Ajoute un article.
      *
-     * @param \Glsr\GestockBundle\Entity\Article $article article à modifier
+     * @return RedirectResponse
      *
-     * @return type
+     * @throws AccessDeniedException
+     */
+    public function addProcessAction()
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+        $article = new Article();
+        // On récupère la requête
+        $request = $this->getRequest();
+        // On crée le formulaire grâce à l'ArticleType
+        $form = $this->createForm(new ArticleType(), $article);
+
+        // On fait le lien Requête <-> Formulaire
+        $form->bind($request);
+
+        // On vérifie que les valeurs rentrées sont correctes
+        if ($form->isValid()) {
+            // On enregistre l'objet $article dans la base de données
+            $etm = $this->getDoctrine()->getManager();
+            $etm->persist($article);
+            $etm->flush();
+            $url = $this->generateUrl(
+                'glstock_art_show',
+                array('name' => $article->getName())
+            );
+            $message = "L'article " . $article->getName() ." est bien ajouté";
+        } else {
+            $url = $this->generateUrl('glstock_art_add');
+            $message = "L'article " . $article->getName() ." n'est pas ajouté !"; 
+        }
+        // On définit un message flash
+        $this->get('session')->getFlashBag()->add('info', $message);
+        // On redirige vers la page de visualisation
+        //  de l'article nouvellement créé
+        return $this->redirect($url);
+    }
+    
+    /**
+     * Modification d'un article.
+     *
+     * @param Article $article article à modifier
+     *
+     * @return Response/RedirectResponse
      */
     public function editAction(Article $article)
     {
         if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            // On définit un message flash
-            $this->get('session')
-                ->getFlashBag()
-                ->add(
-                    'info',
-                    'Vous devez être connecté pour accéder à cette page.'
-                );
-
-            // On redirige vers la page de connexion
-            return $this->redirect(
-                $this->generateUrl(
-                    'fos_user_security_login'
-                )
-            );
+            throw new AccessDeniedException();
         }
         // On crée le formulaire grâce à l'ArticleType
         $form = $this->createForm(new ArticleType(), $article);
@@ -166,8 +157,7 @@ class ArticleController extends Controller
                 $etm->flush();
 
                 // On définit un message flash
-                $this->get('session')
-                    ->getFlashBag()
+                $this->get('session')->getFlashBag()
                     ->add('info', 'Article bien modifié');
 
                 // On redirige vers la page de visualisation
@@ -181,12 +171,6 @@ class ArticleController extends Controller
             }
         }
 
-        // À ce stade :
-        // - soit la requête est de type GET,
-        // donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - soit la requête est de type POST,
-        // mais le formulaire n'est pas valide, donc on l'affiche de nouveau
-
         return $this->render(
             'GlsrGestockBundle:Gestock/Article:edit.html.twig',
             array(
@@ -196,71 +180,75 @@ class ArticleController extends Controller
     }
 
     /**
-     * deleteAction Supprime (désactive) un article.
+     * Supprime (désactive) un article.
      *
-     * @param \Glsr\GestockBundle\Entity\Article $article article à désactiver
+     * @param Article $article article à désactiver
      *
-     * @return type
+     * @return Response
      */
-    public function deleteAction(Article $article)
+    public function deleteShowAction(Article $article)
     {
         if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            // On définit un message flash
-            $this->get('session')
-                ->getFlashBag()
-                ->add('info', 'Vous devez être connecté pour accéder à cette page.');
-
-            // On redirige vers la page de connexion
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+            throw new AccessDeniedException();
         }
-        // On crée un formulaire vide, qui ne contiendra que le champ CSRF
-        // Cela permet de protéger la suppression d'article contre cette faille
         $form = $this->createFormBuilder()->getForm();
 
         //On modifie l'état actif de l'article
         $article->setActive(0);
 
-        $request = $this->getRequest();
-        if ($request->getMethod() == 'POST') {
-            // Si la requête est en POST, on supprimera l'article
-            $form->bind($request);
-
-            if ($form->isValid()) {
-                // On supprime l'article
-                $etm = $this->getDoctrine()->getManager();
-                $etm->persist($article);
-                $etm->flush();
-
-                $this->get('session')
-                    ->getFlashBag()
-                    ->add('info', 'glsr.gestock.article.delete.ok');
-
-                // Puis on redirige vers l'accueil
-                return $this->redirect($this->generateUrl('glstock_home'));
-            } else {
-                $this->get('session')
-                    ->getFlashBag()
-                    ->add('info', 'Article pas désactivé');
-            }
-        }
-
-        // Si la requête est en GET,
-        // on affiche une page de confirmation avant de supprimer
         return $this->render(
             'GlsrGestockBundle:Gestock/Article:delete.html.twig',
             array(
                 'article' => $article,
-                'form' => $form->createView(),
+                'form'    => $form->createView(),
                 )
         );
     }
 
     /**
-     * showAction Affiche un article.
+     * Supprime (désactive) un article.
+     *
+     * @param Article $article
+     *
+     * @return RedirectResponse
+     *
+     * @throws AccessDeniedException
+     */
+    public function deleteProcessAction(Article $article)
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+        $form = $this->createFormBuilder()->getForm();
+
+        $request = $this->getRequest();
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            // On supprime l'article
+            $etm = $this->getDoctrine()->getManager();
+            $etm->persist($article);
+            $etm->flush();
+
+            $this->get('session')
+                ->getFlashBag()
+                ->add('info', 'glsr.gestock.article.delete.ok');
+
+            // Puis on redirige vers l'accueil
+            return $this->redirect($this->generateUrl('glstock_home'));
+        } else {
+            $this->get('session')
+                ->getFlashBag()
+                ->add('info', 'glsr.gestock.article.delete.no');
+        }
+    }
+    
+    /**
+     * Affiche un article.
      *
      * @param \Glsr\GestockBundle\Entity\Article $article article à afficher
      *
-     * @return type
+     * @return Response
      */
     public function showAction(Article $article)
     {
@@ -273,24 +261,44 @@ class ArticleController extends Controller
     }
 
     /**
-     * reassignAction Réassignation d'articles à un autre fournisseur
-     * que celui passé en paramètre.
+     * Réassignation d'articles à un autre fournisseur
+     *   que celui passé en paramètre.
      *
-     * @param \Glsr\GestockBundle\Entity\Supplier $supplier
-     *                                                      Fournisseur dont les articles doivent être réaffectés
+     * @param Supplier $supplier Fournisseur 
+     *   dont les articles doivent être réaffectés
      *
-     * @return type
+     * @return Response/RedirectResponse
+     *
+     * @throws AccessDeniedException
      */
-    public function reassignAction(Supplier $supplier)
+    public function reassignShowAction(Supplier $supplier)
     {
         if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
-            // On définit un message flash
-            $this->get('session')
-                ->getFlashBag()
-                ->add('info', 'Vous devez être connecté pour accéder à cette page.');
+            throw new AccessDeniedException();
+        }
 
-            // On redirige vers la page de connexion
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        // Récupérer la liste des articles à reaffecter
+        $articles = $this->getDoctrine()->getManager()
+            ->getRepository('GlsrGestockBundle:Article')
+            ->getArticleFromSupplier($supplier->getId());
+
+        $form = $this->createForm(new ArticleReassignType(), $articles);
+
+        return $this->render(
+            'GlsrGestockBundle:Gestock/Article:reassign.html.twig',
+            array(
+                'form' => $form->createView(),
+                'articles' => $articles,
+                'supname' => $supplier->getName(),
+                'supid' => $supplier->getId(),
+            )
+        );
+    }
+    
+    public function reassignProcessAction(Supplier $supplier)
+    {
+        if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
         }
 
         // Récupérer la liste des articles à reaffecter
@@ -303,49 +311,36 @@ class ArticleController extends Controller
         // On récupère la requête
         $request = $this->getRequest();
 
-        // On vérifie qu'elle est de type POST
-        if ($request->getMethod() == 'POST') {
-            // On fait le lien Requête <-> Formulaire
-            $form->bind($request);
-            $datas = $form;
+        // On fait le lien Requête <-> Formulaire
+        $form->bind($request);
+        $datas = $form;
 
-            $newArticles = new Article();
-            $newSupplier = new Supplier();
-            $etm = $this->getDoctrine()->getManager();
+        $newArticles = new Article();
+        $newSupplier = new Supplier();
+        $etm = $this->getDoctrine()->getManager();
 
-            foreach ($datas as $data) {
-                $input = explode('-', $data->getName());
-                list($inputName, $articleId) = $input;
-                $inputData = $data->getViewData();
-                if ($inputName === 'supplier') {
-                    $newArticles = $etm->getRepository('GlsrGestockBundle:Article')
-                        ->find($articleId);
-                    $newSupplier = $etm->getRepository('GlsrGestockBundle:Supplier')
-                        ->find($inputData);
-                    //On modifie le fournisseur de l'article
-                    $newArticles->setSupplier($newSupplier);
-                    // On enregistre l'objet $article dans la base de données
-                    $etm->persist($newArticles);
-                    $etm->flush();
-                }
+        foreach ($datas as $data) {
+            $input = explode('-', $data->getName());
+            list($inputName, $articleId) = $input;
+            $inputData = $data->getViewData();
+            if ($inputName === 'supplier') {
+                $newArticles = 
+                    $etm->getRepository('GlsrGestockBundle:Article')
+                    ->find($articleId);
+                $newSupplier = 
+                    $etm->getRepository('GlsrGestockBundle:Supplier')
+                    ->find($inputData);
+                //On modifie le fournisseur de l'article
+                $newArticles->setSupplier($newSupplier);
+                // On enregistre l'objet $article dans la base de données
+                $etm->persist($newArticles);
+                $etm->flush();
             }
-            // On redirige vers la page de visualisation
-            //  de l'article nouvellement créé
-            return $this->redirect(
-                $this->generateUrl(
-                    'glstock_suppli_del',
-                    array('id' => $supplier->getId())
-                )
-            );
         }
-
-        return $this->render(
-            'GlsrGestockBundle:Gestock/Article:reassign.html.twig',
-            array(
-                'form' => $form->createView(),
-                'articles' => $articles,
-                'supname' => $supplier->getName(),
-                'supid' => $supplier->getId(),
+        return $this->redirect(
+            $this->generateUrl(
+                'glstock_suppli_del',
+                array('id' => $supplier->getId())
             )
         );
     }
