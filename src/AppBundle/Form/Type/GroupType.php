c99f401a88d4699e8f91c208685883cb628bd196
Dev-Int <lq@dev-int.net>
2016-03-30 12:01:39 +0200

    [Insight] Commented code should not be committed

--- src/AppBundle/Form/Type/GroupType.php
+++ src/AppBundle/Form/Type/GroupType.php
@@ -37,26 +37,8 @@ class GroupType extends AbstractType
     /**
      * {@inheritdoc}
      */
-//    public function getParent()
-//    {
-//        //return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-//
-//        // Or for Symfony < 2.8
-//        return 'fos_user_group';
-//    }
-
-//    public function getBlockPrefix()
-//    {
-//        return 'app_user_group';
-//    }
-
-    // For Symfony 2.x
-    /**
-     * {@inheritdoc}
-     */
     public function getName()
     {
         return 'group';
-        //return $this->getBlockPrefix();
     }
 }
