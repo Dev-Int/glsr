588911462b48abf92a2298def13cf81d9174271f
Dev-Int <lq@dev-int.net>
2016-05-16 02:50:16 +0200

    Add Tests files

---
+++ src/AppBundle/Tests/Controller/ArticleControllerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ArticleControllerTest extends WebTestCase
+{
+    public function testCreate()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/');
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr'));
+        $crawler = $client->click($crawler->filter('.new_entry a')->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'article[name]' => 'Lorem ipsum dolor sit amet',
+            'article[packaging]' => 10.99,
+            'article[price]' => 10.99,
+            'article[quantity]' => 10.99,
+            'article[minstock]' => 10.99,
+            'article[active]' => true,
+            'article[slug]' => 'Lorem ipsum dolor sit amet',
+                    ));
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $crawler = $client->click($crawler->filter('.record_actions a')->link());
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr'));
+    }
+
+    public function testCreateError()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/new');
+        $form = $crawler->filter('form button[type="submit"]')->form();
+        $crawler = $client->submit($form);
+        $this->assertGreaterThan(0, $crawler->filter('form div.has-error')->count());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testEdit()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/');
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr:contains("First value")'));
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr:contains("Changed")'));
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(1)->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'article[name]' => 'Changed',
+            'article[packaging]' => 10.99,
+            'article[price]' => 10.99,
+            'article[quantity]' => 10.99,
+            'article[minstock]' => 10.99,
+            'article[active]' => true,
+            'article[slug]' => 'Changed',
+            // ... adapt fields value here ...
+        ));
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $crawler = $client->click($crawler->filter('.record_actions a')->link());
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr:contains("First value")'));
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr:contains("Changed")'));
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testEditError()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/');
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(1)->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'article[field_name]' => '',
+            // ... use a required field here ...
+        ));
+        $crawler = $client->submit($form);
+        $this->assertGreaterThan(0, $crawler->filter('form div.has-error')->count());
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testDelete()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr'));
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(0)->link());
+        $client->submit($crawler->filter('form#delete button[type="submit"]')->form());
+        $crawler = $client->followRedirect();
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr'));
+    }    /**
+     * @depends testCreate
+     */
+    public function testSort()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/articles/');
+        $this->assertCount(1, $crawler->filter('table.records_list th')->eq(0)->filter('a i.fa-sort'));
+        $crawler = $client->click($crawler->filter('table.records_list th a')->link());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('table.records_list th a i.fa-sort-up'));
+    }
+}
---
+++ src/AppBundle/Tests/Controller/InventoryControllerTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class InventoryControllerTest extends WebTestCase
+{
+    public function testCreate()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/inventory/');
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr'));
+        $crawler = $client->click($crawler->filter('.new_entry a')->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'inventory[date]' => new \DateTime(),
+            'inventory[active]' => true,
+            'inventory[amount]' => 10.99,
+            'inventory[file]' => 'Lorem ipsum dolor sit amet',
+                    ));
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $crawler = $client->click($crawler->filter('.record_actions a')->link());
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr'));
+    }
+
+    public function testCreateError()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/inventory/new');
+        $form = $crawler->filter('form button[type="submit"]')->form();
+        $crawler = $client->submit($form);
+        $this->assertGreaterThan(0, $crawler->filter('form div.has-error')->count());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testEdit()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/inventory/');
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr:contains("First value")'));
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr:contains("Changed")'));
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(1)->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'inventory[date]' => new \DateTime(),
+            'inventory[active]' => true,
+            'inventory[amount]' => 10.99,
+            'inventory[file]' => 'Changed',
+            // ... adapt fields value here ...
+        ));
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $crawler = $client->click($crawler->filter('.record_actions a')->link());
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr:contains("First value")'));
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr:contains("Changed")'));
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testEditError()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/inventory/');
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(1)->link());
+        $form = $crawler->filter('form button[type="submit"]')->form(array(
+            'inventory[field_name]' => '',
+            // ... use a required field here ...
+        ));
+        $crawler = $client->submit($form);
+        $this->assertGreaterThan(0, $crawler->filter('form div.has-error')->count());
+    }
+
+    /**
+     * @depends testCreate
+     */
+    public function testDelete()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/inventory/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('table.records_list tbody tr'));
+        $crawler = $client->click($crawler->filter('table.records_list tbody tr td .btn-group a')->eq(0)->link());
+        $client->submit($crawler->filter('form#delete button[type="submit"]')->form());
+        $crawler = $client->followRedirect();
+        $this->assertCount(0, $crawler->filter('table.records_list tbody tr'));
+    }
+}
