f6245a7a08baeac16ad498989511168516c6bd2e
Dev-Int <lq@dev-int.net>
2016-09-04 03:37:44 +0200

    [Travis-CI] ErrorException
     - file_put_contents(/home/travis/build/Dev-Int/glsr/bin/dbunit): failed to open stream: No such file or directory

--- bin/dbunit.bat
+++ bin/dbunit.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../vendor/phpunit/phpunit/phpunit
+SET BIN_TARGET=%~dp0/../vendor/phpunit/dbunit/dbunit
 php "%BIN_TARGET%" %*
--- bin/wkhtmltopdf-i386.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET BIN_TARGET=%~dp0/../vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386
-php "%BIN_TARGET%" %*
