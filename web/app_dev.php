0fedf3d225a11a0666031ee643e7826691829525
Dev-Int <lq@dev-int.net>
2016-09-11 18:06:11 +0200

    [Insight] A Symfony application should be bootable

--- web/app_dev.php
+++ web/app_dev.php
@@ -25,6 +25,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
+$kernel->boot();
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
