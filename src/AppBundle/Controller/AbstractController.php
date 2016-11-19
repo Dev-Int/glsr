1a027dad4c0ef694738cd7464ba78360988e541a
Dev-Int <lq@dev-int.net>
2016-11-19 22:03:33 +0100

    [Travis-CI] Fix Sniff violations
     - Expected 1 space after IF keyword; 0 found

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -62,7 +62,7 @@ abstract class AbstractController extends Controller
         $roles = ['ROLE_ADMIN', 'ROLE_SUPER_ADMIN'];
         switch ($entityName) {
             case 'Article':
-                if($this->getUser() !== null && in_array($this->getUser()->getRoles()[0], $roles)) {
+                if ($this->getUser() !== null && in_array($this->getUser()->getRoles()[0], $roles)) {
                     $entities = $etm->getRepository('AppBundle:'.$entityName)->getAllArticles();
                 } else {
                     $entities = $etm->getRepository('AppBundle:'.$entityName)->getArticles();
