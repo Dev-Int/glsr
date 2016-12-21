89b029788c96a94e66c37c34de694a5707f24522
Dev-Int <lq@dev-int.net>
2016-12-21 14:23:56 +0100

    Change parameters type

--- src/AppBundle/Helper/ControllerHelper.php
+++ src/AppBundle/Helper/ControllerHelper.php
@@ -14,7 +14,6 @@
  */
 namespace AppBundle\Helper;
 
-use AppBundle\Entity\Article;
 use Doctrine\Common\Persistence\ObjectManager;
 
 /**
@@ -27,11 +26,11 @@ class ControllerHelper
     /**
      * Tests of creation conditions.
      *
-     * @param \AppBundle\Entity\Article $articles Articles à tester
+     * @param \AppBundle\Entity\Article|array $articles Articles à tester
      * @param \Doctrine\Common\Persistence\ObjectManager $etm Named object manager
      * @return boolean
      */
-    public function testCreate(Article $articles, ObjectManager $etm)
+    public function testCreate($articles, ObjectManager $etm)
     {
         $return = false;
         $orders = $etm->getRepository('AppBundle:Orders')->findAll();
