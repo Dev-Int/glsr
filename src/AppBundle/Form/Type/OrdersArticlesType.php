52296608969ad3cff23d2fdefba9e83eb5ace3e8
Dev-Int <lq@dev-int.net>
2017-01-23 21:23:54 +0100

    [Insight] Unused use statement should be avoided

--- src/AppBundle/Form/Type/OrdersArticlesType.php
+++ src/AppBundle/Form/Type/OrdersArticlesType.php
@@ -18,7 +18,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
