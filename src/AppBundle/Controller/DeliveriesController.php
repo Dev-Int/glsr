200188da2cb6a5e2fbee4e55d9d3dfbf1e57e98a
Dev-Int <lq@dev-int.net>
2017-02-06 18:27:12 +0100

    [Scrutinizer] Comprehensibility - Avoid variables with short names like $em/$qb. Configured minimum length is 3.

--- src/AppBundle/Controller/DeliveriesController.php
+++ src/AppBundle/Controller/DeliveriesController.php
@@ -40,13 +40,13 @@ class DeliveriesController extends AbstractOrdersController
      */
     public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
+        $etm = $this->getDoctrine()->getManager();
         $item = $this->container->getParameter('knp_paginator.page_range');
-        $qb = $em->getRepository('AppBundle:Orders')->createQueryBuilder('o');
-        $qb->where('o.delivdate >= ' . date('Y-m-d'));
-        $qb->andWhere('o.status = 1');
+        $qbd = $etm->getRepository('AppBundle:Orders')->createQueryBuilder('o');
+        $qbd->where('o.delivdate >= ' . date('Y-m-d'));
+        $qbd->andWhere('o.status = 1');
         
-        $paginator = $this->get('knp_paginator')->paginate($qb, $request->query->get('page', 1), $item);
+        $paginator = $this->get('knp_paginator')->paginate($qbd, $request->query->get('page', 1), $item);
         return array(
             'paginator' => $paginator,
         );
