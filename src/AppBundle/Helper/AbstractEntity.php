d69fe2c6f7dcc2b41a0bd4769aba4a0d4f383e8a
Dev-Int <lq@dev-int.net>
2017-04-02 14:49:13 +0200

    Update Documentation

--- src/AppBundle/Helper/AbstractEntity.php
+++ src/AppBundle/Helper/AbstractEntity.php
@@ -1,17 +1,23 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * AbstractEntity Méthodes abstraites des Entity.
+ *
+ * PHP Version 5
+ *
+ * @author    Quétier Laurent <lq@dev-int.net>
+ * @copyright 2014 Dev-Int GLSR
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * @version since 1.0.0
+ *
+ * @link      https://github.com/Dev-Int/glsr
  */
-
 namespace AppBundle\Helper;
 
 /**
- * Description of Entity
+ * Abstract Entity.
  *
- * @author Laurent
+ * @category Helper
  */
 abstract class AbstractEntity
 {
