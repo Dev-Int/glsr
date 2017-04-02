cb69ef861c2cbbec3ffef83d9043e07a8f600e62
Dev-Int <lq@dev-int.net>
2017-04-02 19:19:05 +0200

    [Scrutinizer] Bug - The method addRoles() cannot be called from this context as it is declared private in class AppBundle\Controller\AbstractController.

--- src/AppBundle/Controller/AbstractController.php
+++ src/AppBundle/Controller/AbstractController.php
@@ -91,7 +91,7 @@ abstract class AbstractController extends Controller
         ));
 
         if ($entity === 'Group') {
-            $this->addRoles($form, $entityNew);
+            $this->addRolesAction($form, $entityNew);
         }
         $return = array(strtolower($entity) => $entityNew, 'form'   => $form->createView(),);
 
@@ -116,7 +116,7 @@ abstract class AbstractController extends Controller
             'action' => $this->generateUrl(strtolower($entity).'_create'),
         ));
         if ($entity === 'Group') {
-            $this->addRoles($form, $entityNew);
+            $this->addRolesAction($form, $entityNew);
         }
         $form->handleRequest($request);
         $return = [$entity => $entityNew, 'form' => $form->createView(),];
@@ -151,7 +151,7 @@ abstract class AbstractController extends Controller
             'method' => 'PUT',
         ));
         if ($entityName === 'group') {
-            $this->addRoles($editForm, $entity);
+            $this->addRolesAction($editForm, $entity);
         }
         $deleteForm = $this->createDeleteForm($entity->getId(), $entityName.'_delete');
 
@@ -179,7 +179,7 @@ abstract class AbstractController extends Controller
             'method' => 'PUT',
         ));
         if ($entityName === 'group') {
-            $this->addRoles($editForm, $entity);
+            $this->addRolesAction($editForm, $entity);
         }
         $editForm->handleRequest($request);
         $deleteForm = $this->createDeleteForm($entity->getId(), $entityName.'_delete');
@@ -310,7 +310,7 @@ abstract class AbstractController extends Controller
      * @param \AppBundle\Entity\Group       $group The entity to deal
      * @return \Symfony\Component\Form\Form The form
      */
-    private function addRoles($form, $group)
+    public function addRolesAction($form, $group)
     {
         $form->add('roles', ChoiceType::class, array(
             'choices' => $this->getExistingRoles(),
