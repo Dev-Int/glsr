1c1b4e401fe02c0570b2c8759e630769b273e9d1
Dev-Int <lq@dev-int.net>
2017-04-05 13:12:51 +0200

    Improve StepAction function

--- src/AppBundle/Controller/Install/AbstractInstallController.php
+++ src/AppBundle/Controller/Install/AbstractInstallController.php
@@ -46,33 +46,44 @@ abstract class AbstractInstallController extends Controller
         if ($ctEntity > 0 && $request->getMethod() == 'GET' && is_int($number)) {
             $message = 'gestock.install.st'.$number.'.yet_exist';
         }
-        $form = $this->createForm($typePath, $entityNew, array(
-            'action' => $this->generateUrl('gs_install_st'.$number)
-        ));
+        $form = $this->createForm($typePath, $entityNew, ['action' => $this->generateUrl('gs_install_st'.$number),]);
         if (is_int($number)) {
-            $return = array('message' => $message, 'form' => $form->createView(),);
+            $return = ['message' => $message, 'form' => $form->createView(),];
         } else {
-            $return = array(
-                strtolower($entity) => $entityNew,
-                'form' => $form->createView(),
-            );
+            $return = [strtolower($entity) => $entityNew, 'form' => $form->createView(),];
         }
 
         if ($form->handleRequest($request)->isValid()) {
-            $etm = $this->getDoctrine()->getManager();
-            $etm->persist($entityNew);
-            $etm->flush();
+            $return = $this->validInstall($form, $etm, $number);
 
-            if ($form->get('save')->isClicked()) {
-                $return = $this->redirect($this->generateUrl('gs_install_st4'));
-            } elseif ($form->get('addmore')->isClicked()) {
-                $return = $this->redirect($this->generateUrl('gs_install_st'.$number));
-            } else {
-                $return = $this->redirect($this->generateUrl('gs_install_st'.$number));
-            }
             $this->addFlash('info', 'gestock.install.st'.$number.'.flash');
         }
         
+        return $return;
+    }
+
+    /**
+     * Valid install step.
+     *
+     * @param object                                     $entityNew Entity
+     * @param \Symfony\Component\Form\Form               $form      Form of Entity
+     * @param \Doctrine\Common\Persistence\ObjectManager $etm       Entity Manager
+     * @param integer                                     $number   Number of step install
+     * @return array Route after valid or not
+     */
+    private function validInstall($entityNew, $form, ObjectManager $etm, $number)
+    {
+        $etm->persist($entityNew);
+        $etm->flush();
+
+        if ($form->get('save')->isClicked()) {
+            $return = $this->redirect($this->generateUrl('gs_install_st4'));
+        } elseif ($form->get('addmore')->isClicked()) {
+            $return = $this->redirect($this->generateUrl('gs_install_st'.$number));
+        } else {
+            $return = $this->redirect($this->generateUrl('gs_install_st'.$number));
+        }
+
         return $return;
     }
 }
