3c1b3130b9849a758de6c8438036313edc6c63ef
Dev-Int <lq@dev-int.net>
2017-04-12 01:13:01 +0200

    [Insight] Unused use statement should be avoided

--- src/AppBundle/Entity/Config/Material.php
+++ src/AppBundle/Entity/Config/Material.php
@@ -18,7 +18,6 @@ namespace AppBundle\Entity\Config;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\Mapping\Annotation as Gedmo;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\Common\Collections\ArrayCollection;
 use AppBundle\Entity\UnitStorage;
 use AppBundle\Entity\Article;
