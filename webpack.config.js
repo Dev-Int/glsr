59daf587c153def439a63d3f924aca52a341506b
dev-int <lq@dev-int.net>
2018-05-11 19:18:08 +0200

    Update of webpack-encore

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,16 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
+    // will create public/build/app.js and public/build/app.css
+    .addEntry('app', './assets/js/app.js')
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+    // enable source maps during development
     .enableSourceMaps(!Encore.isProduction())
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
@@ -15,10 +23,8 @@ Encore
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
