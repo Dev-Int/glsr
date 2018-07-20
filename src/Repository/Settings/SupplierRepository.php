58e7de923f30d18771b4769f4559c301550426d1
dev-int <lq@dev-int.net>
2018-07-20 17:16:32 +0200

    Forgotten file during commit 3a023721249460d252167456a94490530ca105de `Add the Supplier entity`

---
+++ src/Repository/Settings/SupplierRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Entity Supplier.
+ *
+ * PHP Version 7
+ *
+ * @author    Quétier Laurent <lq@dev-int.net>
+ * @copyright 2018 Dev-Int GLSR
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * @version GIT: $Id$
+ *
+ * @link https://github.com/Dev-Int/glsr
+ */
+namespace App\Repository\Settings;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * SupplierRepository Entity.
+ *
+ * @category Entity
+ */
+class SupplierRepository extends EntityRepository
+{
+    /**
+     * Returns the active providers corresponding to the logistic family
+     * of the article in parameter.
+     *
+     * @param App\Entity\Settings\Supplier $supplier Selected supplier
+     *
+     * @return \Doctrine\ORM\QueryBuilder DQL query
+     */
+    public function getSupplierForReassign($supplier)
+    {
+        $query = $this->createQueryBuilder('s');
+        $query
+            ->select('s')
+            ->where($query->expr()->neq('s.name', ':idname'))
+            ->andWhere('s.familyLog = :flname')
+            ->andWhere('s.active = true')
+            ->setParameters(['idname' => $supplier->getName(), 'flname' => $supplier->getFamilyLog(),])
+            ->orderBy('s.name', 'ASC');
+
+        return $query;
+    }
+}
