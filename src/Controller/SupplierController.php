62cedaddf0d4f6738906f9fb530f93ba1a24609d
dev-int <lq@dev-int.net>
2018-07-22 23:38:25 +0200

    Replace deletion with deactivation

---
+++ src/Controller/SupplierController.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Settings\Supplier;
+
+/**
+ * Description of SupplierController
+ *
+ * @author dev-int
+ */
+class SupplierController
+{
+    /**
+     * Allows applications to modify the entity associated with the item being
+     * deleted before removing it.
+     *
+     * @param Article $supplier
+     */
+    protected function removeSupplierEntity(Supplier $supplier)
+    {
+        $supplier->setActive(0);
+        $this->em->persist($supplier);
+        $this->em->flush();
+    }
+}
