f4ba4a5e9c1288ab4f8bdafa5ba8f4b577092911
dev-int <lq@dev-int.net>
2018-07-23 22:37:05 +0200

    Part #86 [Supplier selection]  - When choosing the provider, only the family of the provider and the subfamily appear in the select.

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,56 @@
+<?php
+
+
+/**
+ * ArticleController Controller of Article entity.
+ *
+ * PHP Version 7
+ *
+ * @author    Quétier Laurent <lq@dev-int.net>
+ * @copyright 2018 Dev-Int GLSR
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * @version GIT: $Id$
+ *
+ * @link https://github.com/Dev-Int/glsr
+ */
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use App\Entity\Settings\Article;
+
+/**
+ * Article Controller override EasyAdminBundle::AdminController
+ *
+ * @category Controller
+ */
+class ArticleController extends BaseAdminController
+{
+    /**
+     * Allows applications to modify the entity associated with the item being
+     * edited before updating it.
+     *
+     * @param Article $article
+     */
+    public function updateArticleEntity(Article $article)
+    {
+        $article->setUpdateAt(new \DateTime());
+        parent::updateEntity($article);
+    }
+
+    /**
+     * Allows applications to modify the entity associated with the item being
+     * deleted before removing it.
+     *
+     * @param Article $article
+     */
+    protected function removeArticleEntity(Article $article)
+    {
+        $article->setUpdateAt(new \DateTime());
+        $article->setDeleteAt(new \DateTime());
+        $article->setActive(false);
+        $this->em->persist($article);
+        $this->em->flush();
+    }
+}
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -47,16 +47,14 @@ class DefaultController extends Controller
     public function getFamilyLogAction(Request $request)
     {
         $return = new Response('Error');
-        $etm = $this->getDoctrine()->getManager();
+
         if ($request->isXmlHttpRequest()) {
             $familyLog = array();
             $id = $request->get('id');
             if ($id != '') {
-                $supplier = $etm
-                    ->getRepository('App:Settings\Supplier')
-                    ->find($id);
-                // Add directs parents of $familyLog
-                $familyLog['familylog'] = $supplier->getFamilyLog()->getId();
+                // Add directs childrens of $familyLog
+                $familyLog = $this->testLevel($familyLog, $id);
+
                 $response = new Response();
                 $data = json_encode($familyLog);
                 $response->headers->set('Content-Type', 'application/json');
@@ -65,5 +63,41 @@ class DefaultController extends Controller
             }
         }
         return $return;
+    }
+
+    /**
+     * Get the SubFamily of Supplier.
+     *
+     * @param array $familyLog
+     * @param int   $id
+     */
+    protected function getSubFamily(array $familyLog, $id)
+    {
+        // Get all datas
+        $etm = $this->getDoctrine()->getManager();
+        $supplier = $etm->getRepository('App:Settings\Supplier')->find($id);
+        $familyLogs = $etm->getRepository('App:Settings\Diverse\FamilyLog')->findAll();
+
+        // First return data needed
+        $familyLog['id'] = $supplier->getFamilyLog()->getId();
+        $testFamily = '';
+        foreach ($familyLogs as $family) {
+            if ($family->getId() === $familyLog['id']) {
+                $testFamily = $family;
+            }
+        }
+        if ($testFamily->getLevel() === 2) {
+            $familyLog['family']['id'] = $testFamily->getId();
+            $familyLog['family']['name'] = $testFamily->getIndentedName();
+            $key = 0;
+            foreach ($familyLogs as $family) {
+                if ($family->getParent() !== null && $family->getParent()->getId() === $familyLog['id']) {
+                    $familyLog['family'][$key]['id'] = $family->getId();
+                    $familyLog['family'][$key]['name'] = $family->getIndentedName();
+                    $key++;
+                }
+            }
+        }
+        return $familyLog;
     }
 }
