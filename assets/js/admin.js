d79ff348327986b1c8952ab6b0225e101e4a5b65
Laurent Quétier <laurent@PC-2-Debian-9-Laurent>
2019-04-01 11:29:52 +0200

    Install webpack and update assets

---
+++ assets/js/admin.js
@@ -0,0 +1,15 @@
+require('../css/admin.scss');
+
+const $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
