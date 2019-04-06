d7d5f52d1081cf5d109702aa4cfcc43aad46a4bb
Laurent Quétier <laurent@PC-2-Debian-9-Laurent>
2019-04-06 18:00:09 +0200

    [Insight][dev_int/glar#415]  - The Doctrine Entity Managershould not be passed as an arguments  - Th Symfony Dependency Injection Container should not be passed as an argument

--- src/DataFixtures/ORM/LoadUserData.php
+++ src/DataFixtures/ORM/LoadUserData.php
@@ -4,7 +4,7 @@
  *
  * PHP Version 7
  *
- * @author    Quétier Laurent <lq@dev-int.net>
+ * @author    Quétier Laurent <info@developpement-interessant.com>
  * @copyright 2018 Dev-Int GLSR
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
  *
@@ -18,25 +18,21 @@ namespace App\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use App\Entity\Staff\User2;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * LoadUser Data.
  *
  * @category DataFixtures
  */
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
+    protected $encoder;
 
-    public function setContainer(ContainerInterface $container = null)
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
-        $this->container = $container;
+        $this->encoder = $encoder;
     }
 
     /**
@@ -46,15 +42,12 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
      */
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new User2();
+        $userAdmin = new User();
         $userAdmin->setUsername('Admin');
         $userAdmin->setEmail('admin@localhost');
         $userAdmin->setEnabled(true);
-        $userAdmin->setSalt(md5(uniqid()));
 
-        // the 'security.password_encoder' service requires Symfony 2.6 or higher
-        $encoder = $this->container->get('security.password_encoder');
-        $password = $encoder->encodePassword($userAdmin, 'admin');
+        $password = $this->encoder->encodePassword($userAdmin, 'adminadmin');
         $userAdmin->setPassword($password);
         $userAdmin->setRoles(['ROLE_SUPER_ADMIN']);
 
@@ -71,6 +64,6 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
      */
     public function getOrder()
     {
-        return 2;
+        return 1;
     }
 }
