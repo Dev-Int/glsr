fbf92a6e6c18f2f7e2649f3bda66699b7e2df4e7
Laurent Quétier <laurent@PC-2-Debian-9-Laurent>
2019-04-06 20:49:34 +0200

    [Scrutinizer][Bug]  - It seems like $request->getSession() can also be of type null; however, parameter $session of App\Security\LoginFormAu...icator::getTargetPath() does only seem to accept Symfony\Component\HttpFo...ession\SessionInterface, maybe add an additional type check?

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+        if ($targetPath = $this->getTargetPath(/** @scrutinizer ignore-type */ $request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
 
