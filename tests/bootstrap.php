3a09324a29faab69b6af5ab0b592e989b443fef6
Quétier Laurent <Dev-Int@users.noreply.github.com>
2020-11-29 17:05:15 +0100

    Fix sf kernel (#98)
    * fix(insight): fix after SymfonyInsight analyse
    
    * fix(#97): fix Dependabot from github
      - upgrade symfony 5.0.* to 5.1.*
      - close #97
    
    * fix(#98): fix symfony/dotenv deprecation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (!class_exists(Dotenv::class)) {
     $message = 'Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.';
     throw new RuntimeException($message);
-} else {
-    // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__).'/.env');
 }
+
+// load all the .env files
+(new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
