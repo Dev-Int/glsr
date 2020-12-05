d56c25c2605fc9ac8c9ef08a344c42b6512914cb
Dev-Int <devint.creation@gmail.com>
2020-12-05 01:00:20 +0100

    docs: update headers php classes and fix phpcs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require \dirname(__DIR__) . '/vendor/autoload.php';
 
-if (!class_exists(Dotenv::class)) {
+if (!\class_exists(Dotenv::class)) {
     $message = 'Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.';
+
     throw new RuntimeException($message);
 }
 
 // load all the .env files
-(new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->loadEnv(\dirname(__DIR__) . '/.env');
