2d8276ec7837b3f94cc48079a4856b5727e7d8c3
Dev-Int <devint.creation@gmail.com>
2020-12-27 18:08:03 +0100

    chore: install angular project and adapt nginx

---
+++ client/projects/backend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRouting } from './app.routing';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRouting
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
