d030ef9495710b5db0c089d9622d9a880bc7a679
Dev-Int <devint.creation@gmail.com>
2021-02-06 22:56:56 +0100

    fix(client): fix footer style and rename files

--- client/projects/common/modules/footer/footer.component.ts
+++ client/projects/common/modules/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  templateUrl: './footer.template.html',
+  styleUrls: ['./footer.styles.scss'],
 })
 export class FooterComponent {}
