9310f12f96637206174c468ad09353f0eebf86bc
Dev-Int <devint.creation@gmail.com>
2021-04-25 17:32:40 +0200

    tests: update tests with missing use cases, fix some bugs and delete unused files

--- server/tests/Unit/Administration/Infrastructure/Company/Controller/GetCompaniesControllerTest.php
+++ server/tests/Unit/Administration/Infrastructure/Company/Controller/GetCompaniesControllerTest.php
@@ -11,44 +11,42 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace End2End\Tests\Administration\Infrastructure\User\Controller;
+namespace Unit\Tests\Administration\Infrastructure\Company\Controller;
 
-use End2End\Tests\AbstractControllerTest;
-use End2End\Tests\DatabaseHelper;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Unit\Tests\AbstractControllerTest;
+use Unit\Tests\DatabaseHelper;
 
-class GetUsersControllerTest extends AbstractControllerTest
+class GetCompaniesControllerTest extends AbstractControllerTest
 {
     /**
      * @throws \JsonException
      */
-    final public function testGetUsersSuccess(): void
+    final public function testGetCompaniesNoData(): void
     {
         // Arrange
         DatabaseHelper::loadFixtures([['group' => 'user']]);
         $adminClient = $this->createAdminClient();
-        $adminClient->request(Request::METHOD_GET, '/api/administration/users/');
+        $adminClient->request(Request::METHOD_GET, '/api/administration/companies/');
 
         // Act
         $response = $adminClient->getResponse();
 
         // Assert
-        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
-        $content = \json_decode($response->getContent(), true, 512, \JSON_THROW_ON_ERROR);
-        self::assertIsArray($content);
-        self::assertEquals('a136c6fe-8f6e-45ed-91bc-586374791033', $content[0]['uuid']);
+        self::assertSame(Response::HTTP_ACCEPTED, $response->getStatusCode());
+        self::assertSame('No data found', $response->getContent());
     }
 
     /**
      * @throws \JsonException
      */
-    final public function testGetSuppliersSuccess(): void
+    final public function testGetCompaniesSuccess(): void
     {
         // Arrange
-        DatabaseHelper::loadFixtures([['group' => 'user'], ['group' => 'supplier']]);
+        DatabaseHelper::loadFixtures([['group' => 'user'], ['group' => 'company']]);
         $adminClient = $this->createAdminClient();
-        $adminClient->request(Request::METHOD_GET, '/api/administration/suppliers/');
+        $adminClient->request(Request::METHOD_GET, '/api/administration/companies/');
 
         // Act
         $response = $adminClient->getResponse();
